6bbae633541554d38cacde08671c6d5b3a8c62b5
speedy-queen <115551560+speedy-queen@users.noreply.github.com>
2022-11-07 21:16:36 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (6.79%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void preorder(TreeNode* root, vector<int> &v)
+    {
+        if(root==NULL){
+            v.push_back(NULL);
+            return;
+        }
+        
+        preorder(root->left,v);
+        preorder(root->right,v);
+        v.push_back(root->val);
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+        vector<int> v1,v2;
+        
+        preorder(p,v1);
+        preorder(q,v2);
+        
+        return v1==v2;
+    }
+};
