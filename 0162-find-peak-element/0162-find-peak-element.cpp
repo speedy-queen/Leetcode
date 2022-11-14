28edc2ab4bac644e4b9c1ab6463216fb4f687931
speedy-queen <115551560+speedy-queen@users.noreply.github.com>
2022-11-14 22:00:48 +0530

    Time: 7 ms (69.92%), Space: 8.9 MB (56.80%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,36 @@
+// class Solution {
+// public:
+//     int findPeakElement(vector<int>& nums) {
+        
+//         int ans=0;
+//         for(int i=1; i<nums.size(); i++){
+//             if((nums[i] > nums[i-1]) && (nums[i] > nums[i+1]) || i==nums.size()-1){
+//                 ans=i;
+//                 break;
+//             }
+               
+//         }
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if(nums.size()==1)
+            return 0;
+        int ans=0,s=0,e=nums.size()-1;
+        while(s<e)
+        {
+            int mid = s+(e-s)/2;
+            if(nums[mid] < nums[mid+1]){
+                s=mid+1;
+                // break;
+            }
+            else
+                e=mid;
+        }
+        
+        return s;
+    }
+};
