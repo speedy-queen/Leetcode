c5b6dd7d79aea3e320b79681d78d09a531c938e3
speedy-queen <115551560+speedy-queen@users.noreply.github.com>
2022-11-14 22:03:10 +0530

    Time: 11 ms (35.48%), Space: 9 MB (9.74%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -23,12 +23,12 @@ public:
         while(s<e)
         {
             int mid = s+(e-s)/2;
-            if(nums[mid] < nums[mid+1]){
-                s=mid+1;
+            if(nums[mid] > nums[mid+1]){
+                e=mid;
                 // break;
             }
             else
-                e=mid;
+                s=mid+1;
         }
         
         return s;
