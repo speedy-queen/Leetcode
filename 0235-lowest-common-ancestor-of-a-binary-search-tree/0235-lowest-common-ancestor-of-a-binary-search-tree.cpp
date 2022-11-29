25cf0d0e2041fe84d23f60204f1e68acb498881f
speedy-queen <115551560+speedy-queen@users.noreply.github.com>
2022-11-29 22:30:20 +0530

    Time: 87 ms (5.67%), Space: 23.3 MB (19.37%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,19 +10,28 @@
 
 class Solution {
 public:
+    bool solve(TreeNode* root, TreeNode* key, vector<TreeNode*> &arr){
+        if(!root)
+            return false;
+        
+        arr.push_back(root);
+        if(root == key)
+            return true;
+        if(solve(root->left,key, arr) || solve(root->right,key, arr))
+            return true;
+        arr.pop_back();
+        return false;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
-        if(root==NULL)
-            return NULL;
-        if(root==p || root==q)
-            return root;
-        
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-        if(left && right)
-            return root;
-        else if(left && !right) return left;
-        else if(!left && right) return right;
-        else return NULL;
+        vector<TreeNode*> arr1, arr2;
+        solve(root,p,arr1);
+        solve(root,q, arr2);
+        TreeNode* ans=arr1[0];
+        for(int i=0; i<arr1.size() && i<arr2.size(); i++){
+            if(arr1[i] != arr2[i])
+                return ans;
+            ans=arr1[i];
+        }
+        return ans;
     }
 };
